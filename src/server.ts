eb6879942b06034f2550af4a05408379b140a708
Swarup Das <swarupcs@gmail.com>
2026-01-07 19:53:58 +0000

    test lint staged 3

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,9 @@ function login(username: string): string {
     // console.log(username);
     const user = {
         name: "Rakesh",
-    };const name = user["name"];
+    };
+    const name = user.name;
+
     return username + name;
 }
 
