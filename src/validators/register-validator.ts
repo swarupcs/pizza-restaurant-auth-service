b77f57f5035b222f082ed37c203695dee9e7e683
Swarup Das <swarupcs@gmail.com>
2026-01-25 06:45:13 +0000

    test firstname is required

--- src/validators/register-validator.ts
+++ src/validators/register-validator.ts
@@ -6,5 +6,10 @@ export default checkSchema({
         notEmpty: true,
         trim: true,
     },
+    firstName: {
+        errorMessage: "First name is required!",
+        notEmpty: true,
+        trim: true,
+    },
 });
 // export default [body("email").notEmpty().withMessage("Email is required!")];
--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -181,7 +181,25 @@ describe("POST /auth/register", () => {
             expect(users).toHaveLength(0);
         });
 
-        it.todo("should return 400 status code if firstName is missing");
+        it("should return 400 status code if firstName is missing", async () => {
+            // Arrange
+            const userData = {
+                firstName: "",
+                lastName: "K",
+                email: "rakesh@mern.space",
+                password: "secret",
+            };
+            // Act
+            const response = await request(app)
+                .post("/auth/register")
+                .send(userData);
+
+            // Assert
+            expect(response.statusCode).toBe(400);
+            const userRepository = connection.getRepository(User);
+            const users = await userRepository.find();
+            expect(users).toHaveLength(0);
+        });
         it.todo("should return 400 status code if lastName is missing");
         it.todo("should return 400 status code if password is missing");
     });
