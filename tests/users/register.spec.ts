5f3140206aba0b4655c70300b87af0d13e160349
Swarup Das <swarupcs@gmail.com>
2026-01-27 13:23:12 +0000

    test email format

--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -259,7 +259,25 @@ describe("POST /auth/register", () => {
             const user = users[0];
             expect(user.email).toBe("rakesh@mern.space");
         });
-        it.todo("should return 400 status code if email is not a valid email");
+        it("should return 400 status code if email is not a valid email", async () => {
+            // Arrange
+            const userData = {
+                firstName: "Rakesh",
+                lastName: "K",
+                email: "rakesh_mern.space", // Invalid email
+                password: "password",
+            };
+            // Act
+            const response = await request(app)
+                .post("/auth/register")
+                .send(userData);
+
+            // Assert
+            expect(response.statusCode).toBe(400);
+            const userRepository = connection.getRepository(User);
+            const users = await userRepository.find();
+            expect(users).toHaveLength(0);
+        });
         it.todo(
             "should return 400 status code if password length is less than 8 chars",
         );
