301d72c59123eecd3f18781220ec63a16e18a96e
Swarup Das <swarupcs@gmail.com>
2026-01-29 20:01:12 +0000

    remove comment

--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -297,14 +297,13 @@ describe("POST /auth/register", () => {
             const users = await userRepository.find();
             expect(users).toHaveLength(0);
         });
-
         it("shoud return an array of error messages if email is missing", async () => {
             // Arrange
             const userData = {
                 firstName: "Rakesh",
                 lastName: "K",
                 email: "",
-                password: "password", // less than 8 chars
+                password: "password",
             };
             // Act
             const response = await request(app)
