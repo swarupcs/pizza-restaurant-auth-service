55a9e2c60ce8f561141013d904f18bff413b87d1
Swarup Das <swarupcs@gmail.com>
2026-03-06 11:56:22 +0000

    fix typo in tenant controller

--- src/controllers/TenantController.ts
+++ src/controllers/TenantController.ts
@@ -69,15 +69,15 @@ export class TenantController {
         }
 
         try {
-            const tenants = await this.tenantService.getById(Number(tenantId));
+            const tenant = await this.tenantService.getById(Number(tenantId));
 
-            if (!tenants) {
-                next(createHttpError(400, "Tenant id does not exits."));
+            if (!tenant) {
+                next(createHttpError(400, "Tenant does not exist."));
                 return;
             }
 
-            this.logger.info("All tenant have been fetched");
-            res.json(tenants);
+            this.logger.info("Tenant has been fetched");
+            res.json(tenant);
         } catch (err) {
             next(err);
         }
@@ -94,7 +94,9 @@ export class TenantController {
         try {
             await this.tenantService.deleteById(Number(tenantId));
 
-            this.logger.info("All tenant have been fetched");
+            this.logger.info("Tenant has been deleted", {
+                id: Number(tenantId),
+            });
             res.json({ id: Number(tenantId) });
         } catch (err) {
             next(err);
