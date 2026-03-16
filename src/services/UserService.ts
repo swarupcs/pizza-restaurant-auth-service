c97476d2ec58c5f1f34ce3ba6b9ed820e81103ed
Swarup Das <swarupcs@gmail.com>
2026-03-16 08:40:59 +0000

    add tenant id while creating user

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -7,7 +7,14 @@ import createHttpError from "http-errors";
 export class UserService {
     constructor(private userRepository: Repository<User>) {}
 
-    async create({ firstName, lastName, email, password, role }: UserData) {
+    async create({
+        firstName,
+        lastName,
+        email,
+        password,
+        role,
+        tenantId,
+    }: UserData) {
         const user = await this.userRepository.findOne({
             where: { email: email },
         });
@@ -18,7 +25,6 @@ export class UserService {
         // Hash the password
         const saltRounds = 10;
         const hashedPassword = await bcrypt.hash(password, saltRounds);
-
         try {
             return await this.userRepository.save({
                 firstName,
@@ -26,6 +32,7 @@ export class UserService {
                 email,
                 password: hashedPassword,
                 role,
+                tenant: tenantId ? { id: tenantId } : undefined,
             });
         } catch (err) {
             const error = createHttpError(
