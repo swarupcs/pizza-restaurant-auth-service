0ae6d1961b51ca77abecd99cee0a9c7b59b4de0b
Swarup Das <swarupcs@gmail.com>
2026-03-18 15:18:59 +0000

    refactoring remove dups

--- tests/users/create.spec.ts
+++ tests/users/create.spec.ts
@@ -7,6 +7,7 @@ import app from "../../src/app";
 import { Roles } from "../../src/constants";
 import { User } from "../../src/entity/User";
 import { Tenant } from "../../src/entity/Tenant";
+import { createTenant } from "../utils";
 
 describe("POST /users", () => {
     let connection: DataSource;
@@ -34,11 +35,7 @@ describe("POST /users", () => {
     describe("Given all fields", () => {
         it("should persist the user in the database", async () => {
             // Create tenant first
-            const tenantRepository = connection.getRepository(Tenant);
-            const tenant = await tenantRepository.save({
-                name: "Test tenant",
-                address: "Test address",
-            });
+            const tenant = await createTenant(connection.getRepository(Tenant));
 
             const adminToken = jwks.token({
                 sub: "1",
@@ -70,11 +67,7 @@ describe("POST /users", () => {
 
         it("should create a manager user", async () => {
             // Create tenant
-            const tenantRepository = connection.getRepository(Tenant);
-            const tenant = await tenantRepository.save({
-                name: "Test tenant",
-                address: "Test address",
-            });
+            const tenant = await createTenant(connection.getRepository(Tenant));
 
             const adminToken = jwks.token({
                 sub: "1",
