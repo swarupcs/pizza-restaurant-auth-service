57c399530a7ea5ae5833aeb311b8fd5125f3f5fb
Swarup Das <swarupcs@gmail.com>
2026-04-21 18:48:49 +0000

    add user pagination

--- src/types/index.ts
+++ src/types/index.ts
@@ -51,3 +51,8 @@ export interface LimitedUserData {
 export interface UpdateUserRequest extends Request {
     body: LimitedUserData;
 }
+
+export interface UserQueryParams {
+    perPage: number;
+    currentPage: number;
+}
