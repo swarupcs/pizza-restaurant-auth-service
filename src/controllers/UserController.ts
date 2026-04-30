38529706d75332f23f204708c88edb0174c6e594
Swarup Das <swarupcs@gmail.com>
2026-04-30 21:20:47 +0000

    improve error handling middleware

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -19,7 +19,7 @@ export class UserController {
         // Validation
         const result = validationResult(req);
         if (!result.isEmpty()) {
-            return res.status(400).json({ errors: result.array() });
+            return next(createHttpError(400, result.array()[0].msg as string));
         }
 
         const { firstName, lastName, email, password, tenantId, role } =
