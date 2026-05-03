384b9a4152f5259caf03964b14f389e98aa7cc65
Swarup Das <swarupcs@gmail.com>
2026-05-03 03:58:46 +0000

    fix tenantId optional if role admin

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -80,7 +80,7 @@ export class UserService {
                 lastName,
                 role,
                 email,
-                tenant: tenantId ? { id: tenantId } : undefined,
+                tenant: tenantId ? { id: tenantId } : null,
             });
         } catch (err) {
             const error = createHttpError(
