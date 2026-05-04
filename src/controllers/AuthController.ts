1c45e99636a323ab0a7bc3833683666566ed413b
Swarup Das <swarupcs@gmail.com>
2026-05-04 16:24:06 +0000

    add tenant field to JWT

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -123,6 +123,7 @@ export class AuthController {
             const payload: JwtPayload = {
                 sub: String(user.id),
                 role: user.role,
+                tenant: user.tenant ? String(user.tenant.id) : "",
             };
 
             const accessToken = this.tokenService.generateAccessToken(payload);
@@ -169,6 +170,7 @@ export class AuthController {
             const payload: JwtPayload = {
                 sub: req.auth.sub,
                 role: req.auth.role,
+                tenant: req.auth.tenant,
             };
 
             const accessToken = this.tokenService.generateAccessToken(payload);
