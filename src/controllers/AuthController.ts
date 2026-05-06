42e4e9adc5577887776d8340737aee1533a45430
Swarup Das <swarupcs@gmail.com>
2026-05-06 04:49:26 +0000

    change jwt fields, node_env

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -48,6 +48,12 @@ export class AuthController {
             const payload: JwtPayload = {
                 sub: String(user.id),
                 role: user.role,
+                // add tenant id to the payload
+                tenant: user.tenant ? String(user.tenant.id) : "",
+
+                firstName: user.firstName,
+                lastName: user.lastName,
+                email: user.email,
             };
 
             const accessToken = this.tokenService.generateAccessToken(payload);
@@ -64,7 +70,7 @@ export class AuthController {
             res.cookie("accessToken", accessToken, {
                 domain: "localhost",
                 sameSite: "strict",
-                maxAge: 1000 * 60 * 60, // 1h
+                maxAge: 1000 * 60 * 60 * 24 * 1, // 1d
                 httpOnly: true, // Very important
             });
 
@@ -124,6 +130,9 @@ export class AuthController {
                 sub: String(user.id),
                 role: user.role,
                 tenant: user.tenant ? String(user.tenant.id) : "",
+                firstName: user.firstName,
+                lastName: user.lastName,
+                email: user.email,
             };
 
             const accessToken = this.tokenService.generateAccessToken(payload);
@@ -140,7 +149,7 @@ export class AuthController {
             res.cookie("accessToken", accessToken, {
                 domain: "localhost",
                 sameSite: "strict",
-                maxAge: 1000 * 60 * 60, // 1h
+                maxAge: 1000 * 60 * 60 * 24 * 1, // 1d
                 httpOnly: true, // Very important
             });
 
@@ -171,6 +180,9 @@ export class AuthController {
                 sub: req.auth.sub,
                 role: req.auth.role,
                 tenant: req.auth.tenant,
+                firstName: req.auth.firstName,
+                lastName: req.auth.lastName,
+                email: req.auth.email,
             };
 
             const accessToken = this.tokenService.generateAccessToken(payload);
@@ -200,7 +212,7 @@ export class AuthController {
             res.cookie("accessToken", accessToken, {
                 domain: "localhost",
                 sameSite: "strict",
-                maxAge: 1000 * 60 * 60, // 1h
+                maxAge: 1000 * 60 * 60 * 24 * 1, // 1d
                 httpOnly: true, // Very important
             });
 
